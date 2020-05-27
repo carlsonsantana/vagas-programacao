20952f6f9235016d54ece1cc2c91ef8f654fc498
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-05-26 21:41:36 -0300

    Display how many days ago the job oportunity has published

--- src/components/JobItem/index.jsx
+++ src/components/JobItem/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import DateDiff from 'date-diff';
+
 import './styles.css';
 
 export default function JobItem({job}) {
+  const daysAgo = (new DateDiff(new Date(), new Date(job.publishedAt))).days();
+
   return (
     <li class="list-group-item">
       <a class="list-group-item-action" href="#">
         <div class="d-flex w-100 justify-content-between">
           <strong class="mb-1">{job.title}</strong>
-          <small>{job.publishedAt}</small>
+          <small>
+            {daysAgo}
+            {daysAgo > 1 ? ' days ' : ' day '}
+            ago
+          </small>
         </div>
         <p class="mb-1">
           {job.description}
