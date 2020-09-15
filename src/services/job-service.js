7748fd56922b30b1563948964fc7a84d4800d01b
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-09-14 23:34:14 -0300

    Uses API generated ID

--- src/services/job-service.js
+++ src/services/job-service.js
@@ -1,12 +1,9 @@
-import md5 from 'md5';
 import stripHtmlComments from 'strip-html-comments';
 
 const API_URL = 'https://carlsonsantana.github.io/static-jobs-api/data.json';
 
 export function loadJobs() {
-  return requestJobs().then(
-    convertResponseToJSON
-  ).then(createJobsIDs).then(removeMarkdownComments);
+  return requestJobs().then(convertResponseToJSON).then(removeMarkdownComments);
 }
 
 function requestJobs() {
@@ -25,13 +22,6 @@ function checkRequestError(response) {
 
 function convertResponseToJSON(response) {
   return response.json();
-}
-
-function createJobsIDs(jobs) {
-  return jobs.map((job) => {
-    job.id = md5(`${job.url}:${job.publishedAt}`);
-    return job;
-  });
 }
 
 function removeMarkdownComments(jobs) {
